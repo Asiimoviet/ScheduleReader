d80ffd0b7f3a01533e50cc21414780a33a003996
Gordon Chen <Asiimoviet@gmail.com>
2021-04-02 09:17:03 +0800

    Optimized the naming of classes

---
+++ configurator.py
@@ -0,0 +1,63 @@
+import sys
+import json
+import pandas as pd 
+
+config = {
+    'class': '',
+    'selected': []
+}
+
+classes = ['ACT 1', 'ACT 2', 'ACT 3', 'SAT', 'TOEFL 2', 'TOEFL 4']
+
+print('======================================================')
+print('Selected Your Class')
+print('======================================================')
+
+for index in range(len(classes)):
+    print('{}: {}'.format(index + 1, classes[index]))
+
+choice = int(input('Please Input the number before the class you are in: '))
+print('Selected Class: {}'.format(classes[choice - 1]))
+config['class'] = classes[choice - 1]
+
+print('Loading Schedule')
+excel = pd.read_excel(sys.argv[1])
+
+def swipeRow(row: int):
+    rowList = excel.iloc[:, 0].dropna().index
+    for classNum in range(9):
+        rowStart = rowList[classNum]
+        rowEnd = rowList[classNum + 1] - 1
+        periods = excel.iloc[rowStart:rowEnd, row].dropna()
+        found = False
+        for period in periods:
+            if period == '/': continue
+            if period.find(config['class']) != -1: 
+                found = True
+                break
+            for selected in config['selected']:
+                if period.find(selected) != -1:
+                    found = True
+                    break
+            if found: break
+
+        if found: continue
+
+        print('\nSelected the subject you have chosen: ')
+        print('0: I don\'t have a class at this period')
+        for index in range(len(periods)):
+            print('{}: {}'.format(index + 1, periods.iloc[index]))
+        choice = int(input('Please input your choice: '))
+
+        if choice == 0: continue
+        config['selected'].append(periods.iloc[choice - 1])
+        
+    
+swipeRow(2)
+for day in range(4):
+    swipeRow(day + 4) 
+
+print('Exporting...')
+exportFile = open('config.json', 'w+')
+exportFile.write(json.dumps(config))
+exportFile.close()
--- ssap.py
+++ ssap.py
@@ -15,10 +15,11 @@ cal['X-WR-TIMEZONE'] = 'Asia/Shanghai'
 # 读取Excel课程表并导入到DataFrame中
 #path = input("Please Input the path of the excel schedule: ")
 excel = pd.read_excel(sys.argv[1])
-keywords = pd.read_json(sys.argv[2]).loc[:,'classes']
+className = pd.read_json('config.json').loc[:, 'class'].iloc[0]
+keywords = pd.read_json('config.json').loc[:,'selected']
 
 # 处理周一的日期
-monday = datetime.date.fromisoformat(pd.read_json(sys.argv[2]).loc[:,'monday'][0])
+monday = datetime.date.fromisoformat(input('Please Input the date of Monday in format yyyy-MM-dd: '))
 rowIndex = excel.iloc[:, 0].dropna().index
 
 # 提取课程
@@ -27,7 +28,6 @@ def extractClass(timeCol: int, col: int, dayOffset: int):
     print(baseDate.ctime())
     for periodCount in range(9):
         print('')
-        print("======================================================================")
         print('Processing {}th Period'.format(periodCount + 1))
 
         rowStart = rowIndex[periodCount]
@@ -40,13 +40,11 @@ def extractClass(timeCol: int, col: int, dayOffset: int):
         print('Time: {}'.format(timeRaw))
         print('Time Start: {}'.format(timeStart.strftime(timeFormat)))
         print('Time End: {}'.format(timeEnd.strftime(timeFormat)))
-        print("======================================================================")
 
         periodsRaw = excel.iloc[rowStart:rowEnd, col].dropna()
         periods = []
         choice = -1
         
-        print('0: I don\'t have a class at this period')
         for index in range(len(periodsRaw)):
             if periodsRaw.iloc[index] == '/': continue
             if periodsRaw.iloc[index].find(' - S') != -1:
@@ -60,12 +58,14 @@ def extractClass(timeCol: int, col: int, dayOffset: int):
                     'location': ''
                 })
 
-            print('{}: {}'.format(len(periods), periods[len(periods) - 1]['summary']))
-
-            for keyword in keywords:
-                if periods[len(periods) - 1]['summary'].find(keyword) != -1:
-                    choice = len(periods) - 1
-                    break
+            if periods[len(periods) - 1]['summary'].find(className) != -1:
+                choice = len(periods) - 1
+                periods[len(periods) - 1]['summary'] = periods[len(periods) - 1]['summary'][len(className) + 3:]
+            else:
+                for keyword in keywords:
+                    if periodsRaw.iloc[index].find(keyword) != -1:
+                        choice = len(periods) - 1
+                        break
 
         print('')
 
@@ -75,19 +75,29 @@ def extractClass(timeCol: int, col: int, dayOffset: int):
 
         print('Find matching: {}'.format(periods[choice]['summary']))
 
+        # 对名字进行处理 把讲师和class去掉
+        processedSummary = periods[choice]['summary']
+        if processedSummary.find(' - ') != -1:
+            processedSummary = processedSummary[:processedSummary.rfind(' - ')]
+        if processedSummary.find('Class') != -1:
+            processedSummary = processedSummary[:processedSummary.find('Class')]
+        if processedSummary.find('class') != -1:
+            processedSummary = processedSummary[:processedSummary.find('class')]
+
+        print('Name after processing: {}'.format(processedSummary))
+
         period = periods[choice]
         event = Event()
-        event['summary'] = period['summary']
+        event['summary'] = processedSummary
         event['location'] = period['location']
         event['dtstart'] = timeStart.strftime(timeFormat)
         event['dtend'] = timeEnd.strftime(timeFormat)
         cal.add_component(event)
 
-
 extractClass(1, 2, 0)
 for day in range(4):
     extractClass(3, day + 4, day + 1)
-    
+
 # 导出文件
 exportFile = open('result.ics', 'w+')
 exportFile.write(cal.to_ical().decode('UTF-8'))
